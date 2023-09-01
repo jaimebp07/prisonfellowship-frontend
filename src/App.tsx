cbe5cdade4cea0b6880fc5c63fa37575b6dbf271
jaimebp07 <andrescodigo07@gmail.com>
2023-08-31 23:39:09 -0500

    feat: add navbar

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+    <h1>Holaaaa</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Nabvar/Navbar.tsx
@@ -0,0 +1,46 @@
+import MushroomLogo from '../../assets/MushroomLogo.png'
+// import { PublicRoutes } from '../../models/routes';
+// import { useNavigate } from 'react-router-dom';
+
+import '../../styles/components/Navbar.scss'
+
+interface Props {
+  isLoggedIn: boolean
+}
+
+function Navbar(props:Props) {
+  // const navigate = useNavigate();
+
+  const goLogin = () => {
+    // navigate(`/${PublicRoutes.LOGIN}`, { replace: true });
+    console.log("btn ir a LOGIN ")
+
+  }
+  const goLanding = () => {
+    // navigate(`/${PublicRoutes.LANDING}`, { replace: true });
+    console.log("btn ir a LANDING ")
+  }
+  const btnLogIn = () => {
+    return(<button className='buttonLogIn' onClick={goLogin}>LOG IN</button>)
+  }
+
+  const btnBack = () => {
+    return(<button className='buttonLogIn' onClick={goLanding}>SALIR</button>)
+  }
+
+  return (
+    <div className="NavbarContainer">
+      <div className="elementNavbar containerLogo">
+        <img className="mushroomLogo" src={MushroomLogo} />
+      </div>
+      <div className="elementNavbar containerCompanyName">
+        <h2 className="companyNameText">PORTEALBO</h2>
+      </div>
+      <div className='elementNavbar containerButtonLogIn'>
+        {props.isLoggedIn ? btnBack() : btnLogIn()}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
